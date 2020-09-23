9ccc72b08ccb715488f076f54251c37480b86407
andresnoe <andresnoe91@gmail.com>
2020-09-23 15:11:31 +0100

    Updated sam_to_bed for amassing commits

--- sam_to_bed.py
+++ sam_to_bed.py
@@ -23,21 +23,26 @@ BED FILE:
 
 """
 import argparse
+import gzip
 
 # path for samfile
 # samfilepath = "/Users/andresnoe/obds_sep20/working_directory/ERR1755082.test.sam"
 # bedfilepath = "/Users/andresnoe/obds_sep20/working_directory/output.bed"
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description=("Script to convert SAM to BED format"))
 parser.add_argument('--input' , '-i', dest='samfilepath',
                     help='input file path')
 parser.add_argument('--output','-o', dest='bedfilepath',
                     help='output file path')
+parser.add_argument('--pad','-p', dest='bedfilepad',
+                    type =int,
+                    default = 0,
+                    help='number of bases added to start and end of bed file interval')
 args=parser.parse_args()
 
 with open (args.samfilepath, 'r') as samfile:
     #Here samfile is the variable where the samfile will be held in python
-    with open(args.bedfilepath, 'w') as bedfile:
+    with gzip.open(args.bedfilepath, 'wt') as bedfile:
         for line in samfile:
             if line[0] == '@':
                 pass
@@ -47,15 +52,26 @@ with open (args.samfilepath, 'r') as samfile:
                 col = line.split()
                 # chromosome, (col 3 in SAM)
                 chrom = col[2]
+                if chrom == "*":
+                    continue
                 # start position in the chromosome, (col 4 but 0 based)
-                start = (int(col[3])-1)
-                #         end position in the chromosome, (col 3 + len(col 10))
-                end = int(col[4]) + int(len(col[9]))
-                #         name, (col 1)
+                # end position in the chromosome, (col 3 + len(col 10))
+                if args.bedfilepad == 0 :
+                    start = (int(col[3])-1)
+                    end = int(col[4]) + int(len(col[9]))
+                elif args.bedfilepad >0:
+                    start = (int(col[3]) -1 -args.bedfilepad)
+                    # Want to SUBTRACT THE PAD from the start site
+                    end = int(col[4]) + int(len(col[9])) + args.bedfilepad
+                    # Want to ADD THE PAD to the end site
+                # name, (col 1)
                 name = col[0]
-                #         score, (col 5)
+                # score, (col 5)
                 score = col[4]
-                #         strand for a set of genomic positions (hard code ".")
+                if score == 0:
+                    continue
+                # strand for a set of genomic positions (hard code ".")
                 strand = '.'
                 bedfile.write(f'{chrom}\t{start}\t{end}\t{name}\t{score}\t{strand}\n')
 
+# Provide a command line argument to pad the intervals in the bed file
