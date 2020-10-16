c5b9eeee2cb357e4f57cbc19a69d6a6d202a4d23
andresnoe <andresnoe91@gmail.com>
2020-10-16 10:09:24 +0100

    Friday 16/10 commit in AM

--- pca_clustering.R
+++ pca_clustering.R
@@ -1,4 +1,7 @@
 library(tidyverse)
+install.packages("ggthemes")
+library(ggthemes)
+library(cowplot)
 
 # Load data
 
@@ -57,4 +60,95 @@ head(screeplot_df)
 
 screeplot_df[1:20,] %>%
     ggplot(aes(x = PC,  y = percent_var)) +
+    geom_point()
+
+
+# Use the experimental metadata to visualise which cell types visually cluster
+# together in principal component space.
+pca_df <- as.data.frame(count_pca_scaled$x)
+pca_df <- rownames_to_column(pca_df)
+cell_md <- rownames_to_column(cell_md)
+pca_df <- pca_df %>%
+    left_join(cell_md)
+
+str(pca_df)
+
+
+pca_df %>%
+    ggplot(aes(x = PC1, y = PC2, color = rowname)) +
+    geom_point()
+
+
+# https://dplyr.tidyverse.org/reference/tidyeval.html
+for (metadata in factor(colnames(cell_md)[-1])){
+    p <- pca_df %>%
+        ggplot(aes_string(x = "PC1", y = "PC2", color = metadata)) +
+        geom_point()
+    print(p)
+}
+
+ggplot(pca_df) +
+    geom_density(aes(PC1, fill = Status), color = "black", alpha = 0.5) +
+    facet_grid(Time~Infection) +
+    theme_cowplot()
+
+
+# Find the top genes associated with the top principal components.
+# Visualise gene expression values against PC coordinates.
+View(count_pca_scaled$rotation)
+top_genes_PC1 <- count_pca_scaled$rotation[,"PC1"]
+
+top_genes_PC1 <- sort(top_genes_PC1, decreasing = TRUE)
+
+sort(top_genes_PC1, decreasing = TRUE) %>%
+    head
+
+sort(top_genes_PC1, decreasing = TRUE) %>%
+    tail
+
+
+
+top_genes_PC2 <- count_pca_scaled$rotation[,"PC2"]
+
+top_genes_PC2 <- sort(top_genes_PC2, decreasing = TRUE)
+
+sort(top_genes_PC2, decreasing = TRUE) %>%
+    head
+
+sort(top_genes_PC2, decreasing = TRUE) %>%
+    tail
+
+
+#  ============================================
+#  CLUSTERING #
+#  ============================================
+kmean <- kmeans(t(log_count_matrix), centers = 4)
+kmean
+str(kmean)
+
+head(kmeans$cluster, 3)
+
+# View(pca_df)
+# View(kmean)
+
+pca_df$cluster <- as.factor(kmean$cluster[pca_df$rowname])
+pca_df$cluster
+
+pca_df %>%
+    ggplot(aes(x = PC1, y = PC2, color = cluster)) +
+    geom_point()
+
+kmean$withinss
+kmean$betweenss
+
+candidate_k <- 2:20
+km <- sapply(candidate_k, function(i){
+    km <- kmeans(t(log_count_matrix), centers = i)
+    sum(km$withinss)
+})
+str(km)
+
+kmeans_plot <- data.frame(sum_of_withinss = km, k=candidate_k)
+kmeans_plot
+ggplot(kmeans_plot, aes(x=k, y=sum_of_withinss))+
     geom_point()
